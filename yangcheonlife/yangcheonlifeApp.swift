0d23c2914aa93f99c7be7f559e9b288a3d67ff5d
helgisnw <helgisnw@gmail.com>
2024-06-25 13:16:07 +0900

    Initial Commit

---
+++ yangcheonlife/yangcheonlifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  yangcheonlifeApp.swift
+//  yangcheonlife
+//
+//  Created by Woohyun Jin on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct yangcheonlifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
